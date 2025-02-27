3ec5d11d802c733b592aaa144d1495de5d1a856e
NubeEra-ImranAli <nubeera.imranali@gmail.com>
2025-02-27 20:14:42 +0530

    create folder exit code

--- create-folder/verify.sh
+++ create-folder/verify.sh
@@ -3,9 +3,12 @@
 # Path to the folder
 FOLDER_PATH="/etc/abc"
 
+# I just Changed here and Added exit code
 # Check if the folder exists
 if [ -d "$FOLDER_PATH" ]; then
     echo "The folder '$FOLDER_PATH' exists."
+    exit 0
 else
     echo "The folder '$FOLDER_PATH' does not exist."
+    exit 1
 fi
